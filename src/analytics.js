7f62913c0d30961f318ee14bac7b19220f9cbeb4
Mayank Kumar <mayankkumar@outlook.in>
2020-10-11 02:19:14 +0530

    Updated analytics

--- src/analytics.js
+++ src/analytics.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Line } from 'react-chartjs-2';
 import states from './statesList'
 
@@ -9,15 +9,21 @@ const addDays = (date, days) => {
     return date;
 }
 
+
+
 const Analytics = () => {
 
     const [data, setData] = useState({})
-    const [startDate, setStartingDate] = useState('')
-    const [endDate, setEndingDate] = useState('')
+    const [startDate, setStartingDate] = useState('2020-02-28')
+    const [endDate, setEndingDate] = useState('2020-03-28')
     const [gender, setGender] = useState('')
     const [age, setAge] = useState('')
     const [state, setState] = useState('')
 
+
+    useEffect(() => {
+        fetchData()
+    }, []);
     function CreateDataSet(_data, lab, color, bgcolor) {
         return {
             label: lab,
@@ -153,20 +159,16 @@ const Analytics = () => {
     }
 
     async function fetchData(){
-        let deceased = await GetDataSet("Deceased", 'rgb(69, 3, 252)', 'rgba(69, 3, 252, 0.8)');
-        let Recovered = await GetDataSet("Recovered", 'rgb(252, 40, 3)', 'rgba(252, 40, 3, 0.8)')
-        let Hospitalized = await GetDataSet("Hospitalized", 'rgb(0, 112, 0)', 'rgba(0, 112, 0, 0.8)')
+        let deceased = await GetDataSet("Hospitalized", 'rgb(69, 3, 252)', 'rgba(69, 3, 252, 0.8)');
+        let Recovered = await GetDataSet("Deceased", 'rgb(252, 40, 3)', 'rgba(252, 40, 3, 0.8)')
+        let Hospitalized = await GetDataSet("Recovered", 'rgb(0, 112, 0)', 'rgba(0, 112, 0, 0.8)')
         console.log(deceased, Recovered, Hospitalized)
         let newData = {
             datasets: [],
             labels: []
         }
-
         newData.datasets.push(deceased["dataset"], Recovered["dataset"], Hospitalized["dataset"])
         newData.labels.push(...deceased.labels)
-
-
-
         setData(newData)
     }
 
@@ -186,63 +188,8 @@ const Analytics = () => {
                                 display:false,
                             },
                             legend:{
-                                display:false
+                                display:true
                             },
-                            plugins: {
-                                // Change options for ALL labels of THIS CHART
-                                datalabels: {
-                                    color: '#000000',
-                                    align: "top",
-                                    display: "auto"
-                                }
-                            },
-                            layout: {
-                                padding: {
-                                    left: 0,
-                                    right: 0,
-                                    top: 30,
-                                    bottom: 0
-                                }
-                            },
-                            scales:{
-                                xAxes: [{
-                                    gridLines: {
-                                        display:false
-                                    },
-                                    scaleLabel: {
-                                        display: true,
-                                        labelString: 'Days'
-                                    },
-
-                                    type: 'time',
-                                    time: {
-                                        unit:'day',
-                                        displayFormats: {
-                                            day: 'Do MMM'
-                                        }
-                                    },
-                                    distribution: 'linear',
-                                    ticks: {
-                                        autoSkip: true,
-                                        maxTicksLimit: 8
-                                    },
-                                    offset: true
-                                }],
-                                yAxes: [{
-                                    gridLines: {
-                                        display:true
-                                    },
-                                    scaleLabel: {
-                                        display: true,
-                                        labelString: 'Deceased'
-                                    },
-                                    ticks: {
-                                        autoSkip: true,
-                                        maxTicksLimit: 5,
-                                        beginAtZero: true,
-                                    }
-                                }]
-                            }
                         }}
                     />
 
@@ -250,12 +197,12 @@ const Analytics = () => {
 
                 </div>
                 <div className="fil">
-                    <input type="date" placeholder="Start Date" className="searchbox1" onChange={e=>setStartingDate(e.target.value)}/><br/>
-                    <input type="date" placeholder="End Date" className="searchbox1" onChange={e=>setEndingDate(e.target.value)}/><br/>
+                    <input type="date" placeholder="Start Date" className="searchbox1" value={startDate} onChange={e=>setStartingDate(e.target.value)}/><br/>
+                    <input type="date" placeholder="End Date" className="searchbox1" value={endDate} onChange={e=>setEndingDate(e.target.value)}/><br/>
                     <select className="searchbox1" onChange={e=>setGender(e.target.value)}>
                         <option value="">Filter gender</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
                     </select>
                     <select className="searchbox1" onChange={e=>setAge(e.target.value)}>
                         <option value="">Filter age groups</option>
