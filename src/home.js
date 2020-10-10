d941eef0823339e3e4c7d635b449fdd70e48387b
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-10-10 14:23:16 +0530

    component placeholders

---
+++ src/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class helpline extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div style={{textAlign:"center" , color:"white", fontSize: "100px"}}>
+                Home
+            </div>
+        )
+    }
+}
+export default helpline;
