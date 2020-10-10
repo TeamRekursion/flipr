12f08f36778239ba8582dc729c78e89db08c300d
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-10-11 01:11:47 +0530

    filter added

--- src/college.js
+++ src/college.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './college.css';
 // import phone from './Assets/bx-phone.png'
-import wave from './Assets/wave.png'
+// import wave from './Assets/wave.png'
 import {getHospital} from './utils/hospitals'
 import {getState} from './utils/StateUsingIP'
 class college extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            State: 'Delhi'
+            State: 'Delhi',
+            selectValue: "both",
+            selectValue1: "All",
         }
     }
 
@@ -16,7 +18,7 @@ class college extends React.Component{
         this.state.State = await getState();
         let data = await getHospital()
         let StateData = (data.filter(e => {
-            if (e.state == this.state.State) {
+            if (e.state === this.state.State) {
                 return true
             }
             return false
@@ -28,9 +30,49 @@ class college extends React.Component{
         })
     }
     filterCallBack = (e) => {
-        return true
+        if(this.state.selectValue === "both" && this.state.selectValue1 === "All"){
+            return true
+        }
+        else if(this.state.selectValue == "both" && this.state.SelectValue1 != "All"){
+            // console.log(e.state, this.state.selectValue1);
+            if(e.state == this.state.SelectValue1){
+                
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if(this.state.selectValue !== "both" && this.state.SelectValue1 == "All"){
+            if(e.ownership == this.state.SelectValue){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            if(e.ownership == this.state.SelectValue && e.state == this.state.SelectValue1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
-
+    // handleSelect=(e)=>{
+    //     console.log(e);
+    //   }
+      handleChange=(e)=>{
+        // this.setState({selectValue:e.target.value});
+        this.setState({selectValue1:e.target.value});
+        console.log(e.target.value);
+      }
+      handleChange1=(e)=>{
+        // this.setState({selectValue:e.target.value});
+        this.setState({selectValue:e.target.value});
+        console.log(e.target.value);
+      }
 
     render(){
         console.log(this.state)
@@ -40,50 +82,50 @@ class college extends React.Component{
                 <div className="box1">
                     <form>
                     <label for="cars" style={{color:"white"}}>Select state:</label>
-                    <select name="cars" id="cars" className="searchbox1">
-                    <option value="Andhra Pradesh">Andhra Pradesh</option>
-                    <option value="A and N Islands">A & N Islands</option>
-                    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
-                    <option value="Assam">Assam</option>
-                    <option value="Bihar">Bihar</option>
-                    <option value="Chandigarh">Chandigarh</option>
-                    <option value="Chhattisgarh">Chhattisgarh</option>
-                    <option value="Dadar and Nagar Haveli">Dadar and Nagar Haveli</option>
-                    <option value="Daman and Diu">Daman and Diu</option>
-                    <option value="Delhi">Delhi</option>
-                    <option value="Lakshadweep">Lakshadweep</option>
-                    <option value="Puducherry">Puducherry</option>
-                    <option value="Goa">Goa</option>
-                    <option value="Gujarat">Gujarat</option>
-                    <option value="Haryana">Haryana</option>
-                    <option value="Himachal Pradesh">Himachal Pradesh</option>
-                    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
-                    <option value="Jharkhand">Jharkhand</option>
-                    <option value="Karnataka">Karnataka</option>
-                    <option value="Kerala">Kerala</option>
-                    <option value="Madhya Pradesh">Madhya Pradesh</option>
-                    <option value="Maharashtra">Maharashtra</option>
-                    <option value="Manipur">Manipur</option>
-                    <option value="Meghalaya">Meghalaya</option>
-                    <option value="Mizoram">Mizoram</option>
-                    <option value="Nagaland">Nagaland</option>
-                    <option value="Odisha">Odisha</option>
-                    <option value="Punjab">Punjab</option>
-                    <option value="Rajasthan">Rajasthan</option>
-                    <option value="Sikkim">Sikkim</option>
-                    <option value="Tamil Nadu">Tamil Nadu</option>
-                    <option value="Telangana">Telangana</option>
-                    <option value="Tripura">Tripura</option>
-                    <option value="Uttar Pradesh">Uttar Pradesh</option>
-                    <option value="Uttarakhand">Uttarakhand</option>
-                    <option value="West Bengal">West Bengal</option>
+                    <select name="cars" id="cars" className="searchbox1" value={this.state.selectValue1} onChange={this.handleChange}>
+                        <option value="All">All</option>
+                        <option value="Andhra Pradesh">Andhra Pradesh</option>
+                        <option value="A and N Islands">A & N Islands</option>
+                        <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                        <option value="Assam">Assam</option>
+                        <option value="Bihar">Bihar</option>
+                        <option value="Chandigarh">Chandigarh</option>
+                        <option value="Chhattisgarh">Chhattisgarh</option>
+                        <option value="Dadar and Nagar Haveli">Dadar and Nagar Haveli</option>
+                        <option value="Daman and Diu">Daman and Diu</option>
+                        <option value="Delhi">Delhi</option>
+                        <option value="Lakshadweep">Lakshadweep</option>
+                        <option value="Puducherry">Puducherry</option>
+                        <option value="Goa">Goa</option>
+                        <option value="Gujarat">Gujarat</option>
+                        <option value="Haryana">Haryana</option>
+                        <option value="Himachal Pradesh">Himachal Pradesh</option>
+                        <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+                        <option value="Jharkhand">Jharkhand</option>
+                        <option value="Karnataka">Karnataka</option>
+                        <option value="Kerala">Kerala</option>
+                        <option value="Madhya Pradesh">Madhya Pradesh</option>
+                        <option value="Maharashtra">Maharashtra</option>
+                        <option value="Manipur">Manipur</option>
+                        <option value="Meghalaya">Meghalaya</option>
+                        <option value="Mizoram">Mizoram</option>
+                        <option value="Nagaland">Nagaland</option>
+                        <option value="Odisha">Odisha</option>
+                        <option value="Punjab">Punjab</option>
+                        <option value="Rajasthan">Rajasthan</option>
+                        <option value="Sikkim">Sikkim</option>
+                        <option value="Tamil Nadu">Tamil Nadu</option>
+                        <option value="Telangana">Telangana</option>
+                        <option value="Tripura">Tripura</option>
+                        <option value="Uttar Pradesh">Uttar Pradesh</option>
+                        <option value="Uttarakhand">Uttarakhand</option>
+                        <option value="West Bengal">West Bengal</option>
                     </select>
                     <label for="cars" style={{color:"white",marginLeft:"30px"}}>Select ownership:</label>
-                    <select name="cars" id="cars" className="searchbox1">
-                        <option value="volvo">Government</option>
-                        <option value="saab">Private</option>
-                        <option value="opel">Both</option>
-                        
+                    <select name="type" className="searchbox1" value={this.state.selectValue} onChange={this.handleChange1} >
+                        <option value="both">Both</option>
+                        <option value="government">Government</option>
+                        <option value="private">Private</option>
                     </select>
                         {/* <input type="text" placeholder="Filter by type" name="search" className="searchbox1"></input> */}
                         {/* <button className="but">search</button> */}
@@ -91,7 +133,7 @@ class college extends React.Component{
                 </div>
                 {(this.state.StateData) ? (<div className="grid">
                     <div className="large-card1">
-                        <img src={wave} alt="wave" className="wave1"/>
+                        {/* <img src={wave} alt="wave" className="wave1"/> */}
                         <h3 className="text-head">{this.state.StateData["state"]}</h3>
                         <h3 className="text-help">{this.state.StateData["name"]}</h3>
                         <br/>
@@ -152,6 +194,9 @@ class college extends React.Component{
                                             </tr>
                                         )
                                     }
+                                    return(
+                                        null
+                                    )
                                 }
                             ) : null}
                         </table>
