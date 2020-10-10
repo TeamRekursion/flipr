0343149c5526fea8f8f332018c36e9873f56a8e2
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-10-10 20:50:13 +0530

    home page added

--- src/home.js
+++ src/home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import back from './Assets/back.png';
 import map from './Assets/map.png';
+import logo from './Assets/logo.png';
+import {Link} from 'react-router-dom';
 
 class helpline extends React.Component{
     constructor(props) {
@@ -9,14 +11,36 @@ class helpline extends React.Component{
 
         }
     }
+    componentDidMount() {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        console.log("nj")
+        fetch("https://ifconfig.co/json", requestOptions)
+    .then(response => response.json())
+    .then(result => 
+        this.setState({state: result.region_name})
+        )
+    .catch(error => console.log('error', error));
+    }
     render(){
         return(
             <>
             <div className="cont">
                 <img src={back} alt="back" className="bac"/>
                 <div className="gm">
-                    <div>
-                        nj
+                    <div className="no">
+                    <img src={logo} alt="logo"/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <h1 className="suc">A all in one portal to stay<br/> updated about the <br/>pandemic</h1>
+                    <br/>
+                    <br/>
+                    <Link to="/analytics">
+                    <button className="fuc">Explore more!</button>
+                    </Link>
                     </div>
                     <div style={{color:"white"}}>
                         <img src={map} alt="map" className="ma"/>
