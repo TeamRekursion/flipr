f91b0a118a1cd6fc2b8b1a63f17b648a9c9cdc8c
Mayank Kumar <mayankkumar@outlook.in>
2020-10-11 02:58:25 +0530

    fix replace

--- src/college.js
+++ src/college.js
@@ -37,7 +37,10 @@ class college extends React.Component {
             return (e.state === this.state.selectValue1)    
         }
         else if (this.state.selectValue != "All" && this.state.selectValue1 == "All") {
+            if (e.ownership != null)
             return (e.ownership.replace(".","") == this.state.selectValue)
+            else
+            return false    
         }
         else {
             return (e.ownership == this.state.selectValue && e.state == this.state.selectValue1)
